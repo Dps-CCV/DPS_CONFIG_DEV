4fac409e1d5008acadf11ddcc4611bc4f5d26609
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-02-03 13:00:06 +0100

    Url fixed

--- .github/workflows/updateConfig.py
+++ .github/workflows/updateConfig.py
@@ -4,7 +4,7 @@ def main():
   sg = shotgun_api3.Shotgun("https://dareplanet.shotgunstudio.com",
                           script_name="GithubUpdateConfig",
                           api_key=os.environ['UPDATECONFIGKEY'])
-  data = {'descriptor': "sgtk:descriptor:git_branch?branch="+os.environ['BRANCH']+"&path=https://github.com/Dps-CCV/DPS_SHOTGRID_CONFIG_DEV.git&version="+os.environ['NEWCODE']}
+  data = {'descriptor': "sgtk:descriptor:git_branch?branch="+os.environ['BRANCH']+"&path=https://github.com/Dps-CCV/DPS_CONFIG_DEV.git&version="+os.environ['NEWCODE']}
   id = 2542
   sg.update('PipelineConfiguration', id, data)
 
