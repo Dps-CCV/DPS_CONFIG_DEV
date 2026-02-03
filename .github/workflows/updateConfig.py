826c1a8ff10715af81a86db5779b24c6a3beb3c9
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-02-03 11:58:02 +0100

    First commit

---
+++ .github/workflows/updateConfig.py
@@ -0,0 +1,15 @@
+def main():
+  import shotgun_api3
+  import os
+  sg = shotgun_api3.Shotgun("https://dareplanet.shotgunstudio.com",
+                          script_name="GithubUpdateConfig",
+                          api_key=os.environ['UPDATECONFIGKEY'])
+  data = {'descriptor': "sgtk:descriptor:git_branch?branch="+os.environ['BRANCH']+"&path=https://github.com/Dps-CCV/DPS_SHOTGRID_CONFIG_DEV.git&version="+os.environ['NEWCODE']}
+  if os.environ['BRANCH'] == "master":
+    id = 2542
+  sg.update('PipelineConfiguration', id, data)
+
+
+
+if __name__ == '__main__':
+  main()
