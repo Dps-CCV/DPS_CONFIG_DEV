7c5347acd50af8cc38b4afa021ebafeeaf804d1c
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-02-06 11:24:13 +0100

    Publish geo groups test agrupados 11

--- hooks/tk-multi-publish2/maya/collector.py
+++ hooks/tk-multi-publish2/maya/collector.py
@@ -120,7 +120,7 @@ class MayaSessionCollector(HookBaseClass):
         geodivider = item.create_item("maya.session.object_geometry.groupr", "Geometry",
                                              "All Session Geometry")
         geodivider.set_icon_from_path(icon_path)
-        self._collect_object_geo(settings, item, geodivider)
+        self._collect_object_geo(settings, item)
         #self._collect_object_geo_group(settings, item)
         self._collect_particles_geo(settings, item)
         self._collect_ass(settings, item)
@@ -462,16 +462,16 @@ class MayaSessionCollector(HookBaseClass):
     #             # selection set this item represents!
     #             abc_set_item.properties["set_name"] = selection_set
 
-    def _collect_object_geo(self, settings, parent_item, geodivider):
+    def _collect_object_geo(self, settings, parent_item):
         """
         Creates items for each abc set in the scene.
 
         :param parent_item: The maya session parent item
         """
-        self.logger.info("=== STARTING _collect_object_geo ===")
-        self.logger.info("parent_item type: %s" % parent_item.type)
-        self.logger.info("parent_item name: %s" % parent_item.name)
-        self.logger.info("geodivider name: %s" % geodivider)
+        # self.logger.info("=== STARTING _collect_object_geo ===")
+        # self.logger.info("parent_item type: %s" % parent_item.type)
+        # self.logger.info("parent_item name: %s" % parent_item.name)
+        # self.logger.info("geodivider name: %s" % geodivider)
 
         icon_path = os.path.join(self.disk_location, os.pardir, "icons", "geometry.png")
         search = "geo"
@@ -494,7 +494,7 @@ class MayaSessionCollector(HookBaseClass):
                         #     item_types["geometries"] = geodivider
 
                         # self.logger.info("Created Geometry type group" + str(item_types["geometries"]))
-                        geo_object_item = geodivider.create_item(
+                        geo_object_item = parent_item.create_item(
                             "maya.session.object_geo", "Object Geometry", nodeName
                         )
 
