fc128878b04fe0b020451965747b2f414c8337d3
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-02-17 12:16:59 +0100

    borrado y sincronización en Desktop

--- core/hooks/tank_init.py
+++ core/hooks/tank_init.py
@@ -13,7 +13,9 @@ Hook that gets executed every time a new Toolkit API instance is created.
 """
 
 from tank import Hook
-
+import os
+import shutil
+import stat
 
 class TankInit(Hook):
     def execute(self, **kwargs):
@@ -24,7 +26,26 @@ class TankInit(Hook):
 
         The default implementation does nothing.
         """
-        ##synchronize path cache
-        tk = self.parent
-        tk.synchronize_filesystem_structure(full_sync=True)
+        engine = tk.platform.current_engine()
+        if engine.name == "tk-desktop":
+            ##synchronize path cache
+            tk = self.parent
+            tk.synchronize_filesystem_structure(full_sync=True)
+            ##Delete old configs
+            parent_folder = os.path.dirname(os.path.dirname(os.environ['CONFIG']))
+            def fix_permissions(path):
+                for root, dirs, files in os.walk(path, topdown=False):
+                    for d in dirs:
+                        os.chmod(os.path.join(root, d), stat.S_IWUSR)
+                    for f in files:
+                        os.chmod(os.path.join(root, f), stat.S_IWUSR)
+            if os.environ.get("SGD_DESKTOP_SITE_INIT_DONE") == None:
+                for c in os.listdir(parent_folder):
+                    full_path = os.path.join(parent_folder, c)
+                    if full_path not in config:
+                        print(full_path)
+                        os.chmod(full_path, stat.S_IWUSR)
+                        fix_permissions(full_path)
+                        shutil.rmtree(full_path)
+            os.environ["SGD_DESKTOP_SITE_INIT_DONE"] = "1"
         pass
