674bb50f875486fcb9ab732c4ba32924075ff135
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-02-17 13:03:56 +0100

    borrado y sincronización en Desktop 5

--- core/hooks/tank_init.py
+++ core/hooks/tank_init.py
@@ -29,24 +29,25 @@ class TankInit(Hook):
         """
         tk = self.parent
         engine = sgtk.platform.current_engine()
-        if engine and engine.name == "tk-desktop":
-            ##synchronize path cache
-            tk.synchronize_filesystem_structure(full_sync=True)
-            ##Delete old configs
-            parent_folder = os.path.dirname(os.path.dirname(os.environ['CONFIG']))
-            def fix_permissions(path):
-                for root, dirs, files in os.walk(path, topdown=False):
-                    for d in dirs:
-                        os.chmod(os.path.join(root, d), stat.S_IWUSR)
-                    for f in files:
-                        os.chmod(os.path.join(root, f), stat.S_IWUSR)
-            if os.environ.get("SGD_DESKTOP_SITE_INIT_DONE") == None:
-                for c in os.listdir(parent_folder):
-                    full_path = os.path.join(parent_folder, c)
-                    if full_path not in config:
-                        print(full_path)
-                        os.chmod(full_path, stat.S_IWUSR)
-                        fix_permissions(full_path)
-                        shutil.rmtree(full_path)
+        if engine:
+            if engine.name == "tk-desktop":
+                ##synchronize path cache
+                tk.synchronize_filesystem_structure(full_sync=True)
+                ##Delete old configs
+                parent_folder = os.path.dirname(os.path.dirname(os.environ['CONFIG']))
+                def fix_permissions(path):
+                    for root, dirs, files in os.walk(path, topdown=False):
+                        for d in dirs:
+                            os.chmod(os.path.join(root, d), stat.S_IWUSR)
+                        for f in files:
+                            os.chmod(os.path.join(root, f), stat.S_IWUSR)
+                if os.environ.get("SGD_DESKTOP_SITE_INIT_DONE") == None:
+                    for c in os.listdir(parent_folder):
+                        full_path = os.path.join(parent_folder, c)
+                        if full_path not in config:
+                            print(full_path)
+                            os.chmod(full_path, stat.S_IWUSR)
+                            fix_permissions(full_path)
+                            shutil.rmtree(full_path)
             os.environ["SGD_DESKTOP_SITE_INIT_DONE"] = "1"
         pass
